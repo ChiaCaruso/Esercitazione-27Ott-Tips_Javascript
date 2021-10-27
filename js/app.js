a34f85993a625a1303675259505fdf2d42f11a7d
Chiara Caruso <carusochiara08@gmail.com>
2021-10-27 18:33:03 +0200

    secondo backup dell'esercizio

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 // DARK MODE
+
 const darkBtn = document.querySelector(".btn-darkmode");
 darkBtn.addEventListener( "click", () => {
     document.body.classList.toggle("darkMode");
   }
+  );
+
+
+// LUCE HOBBIES
+
+const hobbyBtn = document.querySelector(".btn-hobbies");
+const hobbyDiv = document.querySelector(".hobbies-cont");
+
+hobbyBtn.addEventListener( "click", () => {
+    hobbyDiv.className = "hobbies-hover";
+    },
+    { once: true }
   );
